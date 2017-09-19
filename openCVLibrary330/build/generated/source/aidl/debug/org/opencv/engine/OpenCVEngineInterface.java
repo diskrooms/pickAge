027213ed5bc25a1d7356472914a9b860128bdcaf
diskrooms <diskrooms@gmail.com>
2017-09-19 22:42:53 +0800

    fix

--- openCVLibrary330/build/generated/source/aidl/debug/org/opencv/engine/OpenCVEngineInterface.java
+++ openCVLibrary330/build/generated/source/aidl/debug/org/opencv/engine/OpenCVEngineInterface.java
@@ -1,6 +1,6 @@
 /*
  * This file is auto-generated.  DO NOT MODIFY.
- * Original file: C:\\Users\\jsb-hdp-0\\AndroidStudioProjects\\pickAge\\openCVLibrary330\\src\\main\\aidl\\org\\opencv\\engine\\OpenCVEngineInterface.aidl
+ * Original file: /root/AndroidStudioProjects/pickage/openCVLibrary330/src/main/aidl/org/opencv/engine/OpenCVEngineInterface.aidl
  */
 package org.opencv.engine;
 /**
